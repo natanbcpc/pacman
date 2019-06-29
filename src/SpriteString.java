3aa0f73390f2d9a84f2efd6e1979ebd2225db36d
Thiago Solera <thiagossolera@gmail.com>
2019-06-29 13:07:09 -0300

    Enun to deserialize levels

--- src/SpriteString.java
+++ src/SpriteString.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public enum SpriteString {
+    BLOCK ('#'),
+    DOOR ('d'),
+    PACMAN ('c'),
+    GHOST_RED ('r'),
+    GHOST_BLUE ('b'),
+    GHOST_PINK ('p'),
+    GHOST_ORANGE ('o'),
+    BALL ('.'),
+    SPECIAL_BALL('*');
+
+
+    private final char symbol;
+
+    SpriteString(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
 }
