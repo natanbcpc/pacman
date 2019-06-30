232f97bd8d76e1ac7f7d9d24ebf3a0c35f6ecb9d
Natan Carvalho <natanbcpc@gmail.com>
2019-06-30 15:15:30 -0300

    Restructuring collision strategy architecture

--- src/pacman/models/player/Player.java
+++ src/pacman/models/player/Player.java
@@ -21,11 +21,6 @@ public class Player extends MovingSprite {
     }
 
     @Override
-    public boolean isPlayer() {
-        return true;
-    }
-
-    @Override
     public Image getImage() {
         return this.defaultSprite;
 //        open and close mouth / face correct direction
--- src/pacman/models/player/PlayerCollisionStrategy.java
+++ src/pacman/models/player/PlayerCollisionStrategy.java
@@ -1,21 +1,25 @@
 package pacman.models.player;
 
 import pacman.models.board.Board;
+import pacman.models.ghost.Ghost;
 import pacman.models.sprite.CollisionStrategy;
-import pacman.models.sprite.Sprite;
 import pacman.models.structures.Ball;
 
 public class PlayerCollisionStrategy implements CollisionStrategy<Player> {
+    @Override
+    public void collide(Player self, Player player, Board board) {
 
-    public void collideBall(Player player, Ball ball) {
-        player.addPoints(ball.getPoints());
     }
 
     @Override
-    public void collide(Player player, Sprite other, Board board) {
-        if (other.isBall()) {
-            collideBall(player, ((Ball) other));
-        }
+    public void collide(Player self, Ghost ghost, Board board) {
+
+    }
+
+    @Override
+    public void collide(Player self, Ball ball, Board board) {
+        self.addPoints(ball.getPoints());
+        ball.collide(self, board);
     }
 }
 
